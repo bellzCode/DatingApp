a517374d591ab84d8ceaf6f05596647a3d4fee78
bellzCode <belliappabb@gmail.com>
2019-12-26 15:14:06 +0530

    End of Section 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { tokenName } from '@angular/compiler';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,28 +11,33 @@ import { tokenName } from '@angular/compiler';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
+  // Alertify was added later in section 6, to capture errors.
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
+      // console.log('Logged in successfully');
+      this.alertify.success('login successful');
     }, error => {
       // console.log('Failed to login');
-      console.log(error);
+      // console.log(error);
+      this.alertify.error(error);
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    // const token = localStorage.getItem('token');
+    // return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    // console.log('logged out');
+    this.alertify.message('logged out');
   }
 
 }
